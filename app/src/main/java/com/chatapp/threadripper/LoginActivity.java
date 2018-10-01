41a2dad561f23c530b010f5a76cbc2a3ee5d6402
Huynh Ha <huynhsamha@gmail.com>
2018-10-01 23:42:03 +0700

    add Login Screen

--- app/src/main/java/com/chatapp/threadripper/LoginActivity.java
+++ app/src/main/java/com/chatapp/threadripper/LoginActivity.java
@@ -1,34 +1,24 @@
 package com.chatapp.threadripper;
 
-import android.content.Intent;
 import android.graphics.Color;
 import android.os.Build;
-import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 
-public class Splash extends AppCompatActivity {
+public class LoginActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
+        setContentView(R.layout.activity_login);
 
         if (Build.VERSION.SDK_INT >= 21) {
             getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
         }
         changeStatusBarColor();
-
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                startActivity(new Intent(Splash.this, LoginActivity.class));
-                finish();
-            }
-        }, 1 * 1000);
     }
 
     private void changeStatusBarColor() {
