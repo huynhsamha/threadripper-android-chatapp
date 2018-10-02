9ba8cc451a8950b859d41dc20572f448f9906f2a
Huỳnh Hà <huynhsamha@gmail.com>
2018-10-02 13:24:25 +0700

    fix login screen

--- app/src/main/java/com/chatapp/threadripper/LoginActivity.java
+++ app/src/main/java/com/chatapp/threadripper/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.chatapp.threadripper;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
@@ -7,8 +8,11 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
 
 public class LoginActivity extends BaseActivity {
+
+    Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +23,19 @@ public class LoginActivity extends BaseActivity {
             getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
         }
         changeStatusBarColor();
+
+        initViews();
+    }
+
+    private void initViews() {
+        btnLogin = (Button) findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                finish();
+            }
+        });
     }
 
     private void changeStatusBarColor() {
