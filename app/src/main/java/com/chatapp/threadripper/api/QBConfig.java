ff224c88a8fcaeb76ebad5afba45b66b82989ded
Huỳnh Hà <huynhsamha@gmail.com>
2018-10-18 07:47:33 +0700

    add QB videocall

---
+++ app/src/main/java/com/chatapp/threadripper/api/QBConfig.java
@@ -0,0 +1,8 @@
+package com.chatapp.threadripper.api;
+
+public class QBConfig {
+    public static final String APP_ID = "74224";
+    public static final String AUTH_KEY = "bS5K6K2qMeQKHOu";
+    public static final String AUTH_SECRET = "FPTNRCWaxCF4-Q-";
+    public static final String ACCOUNT_KEY = "LzPrthVNj9ZC1EL-youy";
+}
